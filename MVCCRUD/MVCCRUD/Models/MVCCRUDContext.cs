229725cf5b921cb8b281678729ecbf563ff02272
natadeveloper20 <mendietanatanael9@gmail.com>
2024-08-07 02:06:02 -0300

    Proyecto

---
+++ MVCCRUD/MVCCRUD/Models/MVCCRUDContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MVCCRUD.Models
+{
+    public partial class MVCCRUDContext : DbContext
+    {
+        public MVCCRUDContext()
+        {
+        }
+
+        public MVCCRUDContext(DbContextOptions<MVCCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=(local); DataBase=MVCCRUD;Integrated Security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.Property(e => e.Clave)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Fecha).HasColumnType("date");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(60)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
